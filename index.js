07aa63655284b8cdc050dadbc6d406bc06d45958
julijajarmale <julija.jarmale@gmail.com>
2022-03-29 15:09:59 +0300

    namu darbai 0329

---
+++ index.js
@@ -0,0 +1,55 @@
+//1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+const pirmas = 1;
+console.log('pirmas', pirmas);
+
+const antras = 2;
+console.log('antras', antras);
+
+const trecias = 3;
+console.log('trecias', trecias);
+
+//2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+
+const pirmasMenuo = 'sausis';
+console.log('pirmas menuo', pirmasMenuo);
+
+const antrasMenuo = 'vasaris';
+console.log('antras menuo', antrasMenuo);
+
+const treciasMenuo = 'kovas';
+console.log('trecias menuo', treciasMenuo);
+
+// 3.Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+
+const kaina = [2.99, 5.80, 8.99, 4.99, 6];
+console.log('kainos skirtingose parduotuvese', kaina);
+
+const vaikuSkaiciusSeimoje = [0, 1, 3, 1, 5];
+console.log('vaiku skaicius seimoje', vaikuSkaiciusSeimoje);
+
+const dienosTemp = [15, 18, 13, 18, 20];
+console.log('auksciausia dienos temperatura', dienosTemp);
+
+//4.Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+
+const pavarde = ['Jonaitis', 'Petraitis', 'Simasius', 'Simpsonas', 'Burokas'];
+console.log('Susrinkimo dalyviai', pavarde);
+
+const valstybes = ['Lietuva', 'Lenkija', 'Latvija', 'Estija', 'Suomija'];
+console.log('Valstybiu sarasas', valstybes);
+
+const parduotuves = ['Maxima', 'Rimi', 'IKI', 'Norfa', 'Lidl'];
+console.log('Parduotuviu sarasas', parduotuves);
+
+//Veiksmai su kintamaisiais
+
+//1. Susumuoti visus skaičiaus tipo kintamuosius
+
+const sum = pirmas + antras + trecias;
+console.log('the sum of ' + pirmas + ',' + antras + ' and ' + trecias + ' is: ' + sum);
+
+//2. Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+
+const tekstoElementai = (pirmasMenuo + ' ' + antrasMenuo + ' ' + treciasMenuo);
+console.log(tekstoElementai);
+
